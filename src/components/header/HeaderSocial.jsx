e75924a2940a8edbce9135be1f91a8e5f1c2af25
sanasainath <sanasainath013@gmail.com>
2024-06-19 10:49:02 +0530

    folder changes

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import {BsLinkedin} from 'react-icons/bs';
-import {FaGithub} from 'react-icons/fa'
-import {FiDribbble} from 'react-icons/fi'
+import React from 'react';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
+import { FiDribbble } from 'react-icons/fi';
+
 const HeaderSocial = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/sana-sainath-893a8b26b/" target='_blank'><BsLinkedin/></a>
-        <a href="https://github.com" target='_blank'><FaGithub/></a>
-        <a href="https://facebook.com" target='_blank'><FiDribbble/></a>
-
-      
+      <a href="https://www.linkedin.com/in/sana-sainath-893a8b26b/" target='_blank' rel="noopener noreferrer"><BsLinkedin /></a>
+      <a href="https://github.com" target='_blank' rel="noopener noreferrer"><FaGithub /></a>
+      <a href="https://facebook.com" target='_blank' rel="noopener noreferrer"><FiDribbble /></a>
     </div>
-  )
+  );
 }
 
-export default HeaderSocial
+export default HeaderSocial;
