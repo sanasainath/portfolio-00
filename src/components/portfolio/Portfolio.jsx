3134389a4f3dc24f2a5fc7c0177f6e425cd7a1fe
sanasainath <sanasainath013@gmail.com>
2024-07-09 17:25:42 +0530

    project adding

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -29,20 +29,28 @@ const Portfolio = () => {
   </article>
   <article className='portfolio__item'>
     <div className='portfolio__item-image'>
-    <img src='' alt=''/>
+    <img src='/images/Screenshot 2024-07-09 142559.png' alt=''/>
     </div>
-      <h3>This is a portfolio item </h3>
-
+      <h3> </h3>
+      <div className='check_components'>
+    <span className='badge react'>React</span>
+    <span className='badge bootstrap'>TailWind Css</span>
+    <span className='badge node'>Node Js</span>
+    <span className='badge mongodb'>MongoDB</span>
+    <span className='badge mongodb'>Socket IO</span>
+    
+    
+</div>
       <div className='portfolio__item-cta'>
-      <a href='https://github.com/sanasainath/Ecommerce-Shopping-Cart' className='btn'>GitHub</a>
-      <a href='https://github.com/sanasainath/Ecommerce-Shopping-Cart' className='btn btn-primary'>Live Demo</a>
+      <a href='https://github.com/sanasainath/MernChat-App' className='btn'>GitHub</a>
+      <a href='https://chat-app-frontend-roan.vercel.app' className='btn btn-primary'>Live Demo</a>
       </div>
   
 
   </article>
   <article className='portfolio__item'>
     <div className='portfolio__item-image'>
-    <img src='/images/Screenshot 2024-06-02 231329.png' alt='About Img' />
+    <img src='' alt='About Img' />
     </div>
       <h3>This is a portfolio item title</h3>
       <div className='portfolio__item-cta'>
